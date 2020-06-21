40efd73e18fb5ca7011d9d1b061d1ad8306d77c7
sangwon21 <junothyme@gmail.com>
2020-06-21 18:07:00 +0900

    feat: Made Header Component
    - Made Header Component to Route Shop Page
    - Placed Header Component Outside of Switch Section to Persist on
    Regardless of Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import "./App.css";
 import HomePage from "./pages/Home/HomePage";
 import ShopPage from "./pages/Shop/Shop";
+import Header from "./components/Header/Header";
+import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
