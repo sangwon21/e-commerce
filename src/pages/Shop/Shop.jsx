71be12bdc326875d0a08b828897fcd058c6c52a1
sangwon21 <junothyme@gmail.com>
2020-06-23 00:32:17 +0900

    feat: Moved Shop Data into Redux Store
    - For the Sake of Convenience in Testing, Shop Data is moved to the
    Redux Store

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,26 +1,13 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./Shop.data";
-import CollectionPreview from "../../components/Collection-Preview/CollectionPreview";
+import React from "react";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import CollectionOverview from "../../components/CollectionOverview/CollectonOverview";
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => {
+  return (
+    <div className="shop-page">
+      <CollectionOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
