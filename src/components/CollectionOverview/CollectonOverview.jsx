11e7c169e6dab305e804449a5bac16f6b9e1a60c
sangwon21 <junothyme@gmail.com>
2020-06-23 12:58:03 +0900

    feat: Created Selectors and Updated Corresponding Components
    - Moved the Control of the Data Flow from Local States to Redux Store

--- src/components/CollectionOverview/CollectonOverview.jsx
+++ src/components/CollectionOverview/CollectonOverview.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../Collection-Preview/CollectionPreview";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 import "./CollectionOverview.styles.scss";
 
@@ -16,7 +16,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
