cc832194edf39eb129dc3a04b977fd7e2771df61
sangwon21 <junothyme@gmail.com>
2020-06-27 15:52:36 +0900

    feat: Added Clear Cart Action to redux-saga
    - Added Clearing Cart Action to redux-saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 
 import ShopActionTypes from "./shop.types";
 import {
@@ -27,4 +27,8 @@ export function* fetchCollections() {
 
 export function* fetchCollectionsStart() {
   yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
